071a7d03ae919ddbd3c0ecbbbf4c4fb96cd2a588
Miguel Armas <miguel.armas@jycleadingcorp.com>
2021-01-23 00:28:32 -0500

    master: se agregaron etiquetas

--- services/webscraping_service.py
+++ services/webscraping_service.py
@@ -41,9 +41,6 @@ class WebScrapingService():
     def scrape_request(self, cadena_busqueda):
         pagina_web = GOOGLE_JOBS["WS_PORTAL_LABORAL"]               ## google jobs
         numero_paginas = GOOGLE_JOBS["WS_PAGINAS"]                  ## 0
-        numero_pagina_inicio = GOOGLE_JOBS["WS_PAGINA_INICIAL"]     ## 1
-        numero_ofertas = GOOGLE_JOBS["WS_OFERTAS"]                  ## None
-        area_filtro = GOOGLE_JOBS["WS_AREA"]                        ## None
         url_pagina = GOOGLE_JOBS["WS_PORTAL_LABORAL_URL"]           ## https://google.com
 
         ## incrusto mi keyword search en la url
@@ -60,41 +57,42 @@ class WebScrapingService():
             1                           # id_keyword
         )
         id_webscraping_insert = self.insert_then_return_latest_row(wscraping)
-        listaOferta = self.scrape(url_pagina, url_busqueda, "", numero_pagina_inicio, numero_paginas, numero_ofertas, id_webscraping_insert)
+        self.scrape(url_busqueda, numero_paginas, id_webscraping_insert)
 
 
-    def scrape(self, url_principal, prefix_url, sufix_url, pagina_inicial, cant_paginas, cant_ofertas, id_carga):
-        lista_oferta = []
-        list_clean = []
+    def scrape(self, url_pagina, numero_paginas, id_webscraping):
 
-        # CADENA_BUSQUEDA = "analista programador"
+        # inicio el driver
         driver = webdriver.Chrome('./chromedriver.exe')
-        driver.get(prefix_url)
-        scroll_empleos = driver.find_element_by_class_name("vWdgBe")  # elemento que contiene la lista de empleos
+        driver.get(url_pagina)
 
-        for x in range(0, cant_paginas):
+        # scroll
+        scroll_empleos = driver.find_element_by_class_name("vWdgBe")  # lista de empleos
+        for x in range(0, numero_paginas):
             scroll_empleos.send_keys(Keys.END)
             sleep(0.5)
-
         scroll_empleos.send_keys(Keys.UP)
 
-        grupos_de_empleos = driver.find_elements_by_class_name(
-            "nJXhWc")  # elemento que contiene cada grupo de 10 empleos
-        titulos = driver.find_elements_by_class_name("BjJfJf")  # elemento que contiene cada titulo
+        # obtengo los campos del modelo oferta
+        titulos = driver.find_elements_by_class_name("BjJfJf")  # items
 
-        for x in titulos:
-            oferta = {}
-            detalle2 = ""
-            # print(x.text)
-            c_detalle = driver.find_element_by_class_name("jolnDe")
-            x.click()
+        for titulo in titulos:
+            item_detalle = driver.find_element_by_class_name("jolnDe")
+            titulo.click()
             sleep(random.uniform(0.5, 1))
-            etiquetas = c_detalle.find_elements_by_class_name("sMzDkb")
-            url = c_detalle.find_element_by_class_name("pMhGee").get_attribute('href')
-            detalle = c_detalle.find_element_by_class_name("HBvzbc")
+            etiquetas = item_detalle.find_elements_by_class_name("sMzDkb")
+            url_oferta = item_detalle.find_element_by_class_name("pMhGee").get_attribute('href')
+            detalle = item_detalle.find_element_by_class_name("HBvzbc")
 
+            detalle2 = ""
+            tiempo_publicado = ""
+            empresa = ""
+            lugar = ""
             try:
-                masInfo = c_detalle.find_element_by_class_name("cVLgvc")
+                tiempo_publicado = item_detalle.find_element_by_class_name("SuWscb").text
+                masInfo = item_detalle.find_element_by_class_name("cVLgvc")
+                empresa = item_detalle.find_element_by_class_name("nJlQNd").text
+                lugar = item_detalle.find_element_by_class_name("tJ9zfc").text
                 masInfo.click()
                 detalle2 = detalle.find_element_by_class_name("WbZuDe").text
                 print("s" + detalle2)
@@ -102,56 +100,52 @@ class WebScrapingService():
                 pass
 
             ofer = ofertaModelo.Oferta(
-                id_carga,                   # id_webscraping
-                'titulos test',
-                'empresa test',
-                'lugar test',
-                'tiempo publicado test',
-                'salario test',
-                'modalidad trabajo test',
-                'subarea test',
-                url,                        # url oferta
-                prefix_url,                 # url pagina
-                x.text,                     # puesto? -> area
-                datetime.now(),
-                datetime.now(),
-                detalle.text + detalle2,    # detalle
-                datetime.now()
+                id_webscraping,                 # id_webscraping
+                titulo.text,                    # titulo
+                empresa,                        # empresa
+                lugar,                          # lugar
+                tiempo_publicado,               # tiempo publicado
+                'salario test',                 # salario
+                'modalidad trabajo test',       # modalidad
+                'subarea test',                 # subarea
+                url_oferta,                     # url oferta
+                url_pagina,                     # url pagina
+                'area test',                    # area
+                datetime.now(),                 # fecha creacion
+                datetime.now(),                 # fecha modificacion
+                detalle.text + detalle2,        # detalle
+                datetime.now()                  # fecha publicacion
             )
 
             try:
                 ofer.setEmpresa(etiquetas[0].text)
                 ofer.setLugar(etiquetas[1].text)
             except:
-                oferta["lugar"] = ""
-            lista_oferta.append(oferta)
+                pass
 
             id_oferta_insert = self.__of_service.insert_then_return_latest_row(ofer)
-            #id = controller.registrar_oferta(con, oferta)
+            parrafo = detalle.text.splitlines()
+            self.__of_detalle_service.insert_then_return_latest_row(self.limpiarParrafo(parrafo, id_oferta_insert))
 
-            PARRAFO = detalle.text.splitlines()
-
-            for line in PARRAFO:
-                line = line.strip()
-                if (len(line) > 0):
-                    if (not line[0].isalpha()):
-                        line = line[1:]
-                    line = line.strip().upper()
-                    line = re.sub(r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1",
-                                  normalize("NFD", line), 0, re.I)
-                    if (not line == ""):
-                        ofer_detalle = oferta_detalle.OfertaDetalle(
-                            id_oferta_insert,               # id_oferta
-                            line,                           # descripcion
-                            'descripcion normalizada test',
-                            3,
-                            5,
-                            datetime.now(),
-                            datetime.now(),
-                            1
-                        )
-
-            id_oferta_detalle_insert = self.__of_detalle_service.insert_then_return_latest_row(ofer_detalle)
-        return lista_oferta
+    def limpiarParrafo(self, parrafo, id_oferta):
+        for descripcion in parrafo:
+            descripcion = descripcion.strip()
+            if (len(descripcion) > 0):
+                if (not descripcion[0].isalpha()):
+                    descripcion = descripcion[1:]
+                descripcion = descripcion.strip().upper()
+                descripcion = re.sub(r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1",
+                              normalize("NFD", descripcion), 0, re.I)
+                if (not descripcion == ""):
+                    return oferta_detalle.OfertaDetalle(
+                        id_oferta,                          # id_oferta
+                        descripcion,                        # descripcion
+                        'descripcion normalizada test',     # descripcion normalizada
+                        3,                                  # ind_activo
+                        5,                                  # modo_inactivo
+                        datetime.now(),                     # fecha_creacion
+                        datetime.now(),                     # fecha_modificacion
+                        1                                   # ofertaperfil_id
+                    )
 
 WebScrapingService().iterar_scrape()
